99bedfd7bc6c81fcdee41fd4ebd90241b0d2cfe8
antoniofs23 <af3036@nyu.edu>
2022-11-16 11:44:06 -0600

    merged postprocessing class in pupilcore class

--- run_pupil_processing.py
+++ run_pupil_processing.py
@@ -1,9 +1,9 @@
-import pupil_postprocessing
+import pupilcore as pupil
 
-postprocessing = pupil_postprocessing.pupil_postprocessing(
+postprocessing = pupil.pupil_postprocessing(
     path                ='/home/antonio/WORKSPACE/test_data/ant_001_2022-11-11_17h36.59.762/000',
-    start_time          =24,
-    ang                 = 1.5,
+    start_time          =24,   #time to exclude before experiment begins
+    ang                 = 1.5, #allowed eye-jitter in dva
     screen_width        = 38,
     viewing_distance    = 57,
     resolution          = [1024,768]
@@ -15,7 +15,7 @@ postprocessing.print_file_struc()
 # get gaze data
 gaze_data = postprocessing.get_gaze_pos()
 
-# remove low confidence samples
+# remove low confidence samples < 0.95
 new_data = postprocessing.remove_low_conf(gaze_data)
 
 # plot data again
